e61e8e233151fb161eb7a6f2b989d44abf5de0f0
neptunobkp <116418159+neptunobkp@users.noreply.github.com>
2022-10-21 23:44:14 -0500

    Add files via upload

---
+++ DemonioContactador/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemonioContactador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WSExecutorContactador.EjecutarTareas serviceContext = new DemonioContactador.WSExecutorContactador.EjecutarTareas();
+                //String resultado = serviceContext.EjecutarTareasPendientes();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
